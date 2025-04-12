d3e5016cc0615b9b9ae632e3c9401ead5e4e2714
Jaeson Joseph <jaeson.joseph@siemens-healthineers.com>
2025-04-13 01:05:09 +0530

    Buiild V2

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
@@ -30,11 +30,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 504, hash: 'ce939fda0d78d4b76ad09053bf402b79cc887b12a6d504be10eacc74a7b3d470', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1017, hash: 'd1107c716debe0123f2b852a91b9f10010d49fa5f40cec835cafe64fb469dd43', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 4620, hash: '509db84539c812f26fd521a3b425540a0d55d28388c98d229421febbc77e375f', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 5102, hash: 'e7acb742b6668b3b4438b16bf84bb25d24db22d8fd15ca5f5934457b642c8a1e', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'human/index.html': {size: 31631, hash: 'a215411dfbc380a5104eb38f00ce57689b5ccebd555dbde26fbbc2d5b9ca33a4', text: () => import('./assets-chunks/human_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 505, hash: 'e83a79703e87791ed9d8d68dae46549c624f9bcfad0a7304cd5f92b46a257047', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1018, hash: '4717b9fc29de595ca2e4983a40a10516e9b30d9a127c53d74f5a4c96cea968a2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 5106, hash: '6bafdc72abab68349cb5bec0f09e19970f61009158b8857bb61909511637d03f', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 4624, hash: '945a1a9f063822499c18cdddfb812c9682b40557971621bbe945abdb57e39c79', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'human/index.html': {size: 31635, hash: 'c65da38c1255f835839dc43437f78c380cecf7ed519ec9a86d3d9d757210e0c4', text: () => import('./assets-chunks/human_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
